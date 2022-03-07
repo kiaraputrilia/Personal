704edadb0295fa27c18f96157026cf9cfa409145
Kiara Putrilia <kiaraputrilia@gmail.com>
2022-03-07 00:18:48 -0500

    letter

---
+++ script.js
@@ -0,0 +1,127 @@
+$(document).ready(function(){
+		   
+    $( '#showTitleOne' ).hover(function() {
+        if($( '#titleOne' ).is(":visible")){
+            $( '#titleOne' ).fadeOut(200);
+            $(".highlight, .active").css({
+            "color": 'rgb(100, 70, 255)'
+        }).removeClass("night");		   
+        } else{
+            $( '#titleOne' ).fadeIn();
+            $(".highlight, .active").css({
+            "color": "black"
+        }).addClass("night");		   
+        }
+     });
+     
+    });
+
+    $(document).ready(function(){
+       
+    $( '#showTitleTwo' ).hover(function() {
+        if($( '#titleTwo' ).is(":visible")){
+            $( '#titleTwo' ).fadeOut(200);
+            $(".highlight, .active").css({
+            "color": 'rgb(100, 70, 255)'
+        }).removeClass("night");		   
+        } else{
+            $( '#titleTwo' ).fadeIn();
+            $(".highlight, .active").css({
+            "color": "black"
+        }).addClass("night");		   
+        }
+     });
+     
+    });		
+    
+    $(document).ready(function(){
+       
+    $( '#showTitleThree' ).hover(function() {
+        if($( '#titleThree' ).is(":visible")){
+            $( '#titleThree' ).fadeOut(200);
+            $(".highlight, .active").css({
+            "color": 'rgb(100, 70, 255)'
+        }).removeClass("night");		   
+        } else{
+            $( '#titleThree' ).fadeIn();
+            $(".highlight, .active").css({
+            "color": "black"
+        }).addClass("night");		   
+        }
+     });
+     
+    });			
+
+    $(document).ready(function(){
+       
+    $( '#showTitleFour' ).hover(function() {
+        if($( '#titleFour' ).is(":visible")){
+            $( '#titleFour' ).fadeOut(200);
+            $(".highlight, .active").css({
+            "color": 'rgb(100, 70, 255)'
+        }).removeClass("night");		   
+        } else{
+            $( '#titleFour' ).fadeIn();
+            $(".highlight, .active").css({
+            "color": "black"
+        }).addClass("night");		   
+        }
+     });
+     
+    });				
+
+    $(document).ready(function(){
+       
+    $( '#showTitleFive' ).hover(function() {
+        if($( '#titleFive' ).is(":visible")){
+            $( '#titleFive' ).fadeOut(200);
+            $(".highlight, .active").css({
+            "color": 'rgb(100, 70, 255)'
+        }).removeClass("night");		   
+        } else{
+            $( '#titleFive' ).fadeIn();
+            $(".highlight, .active").css({
+            "color": "black"
+        }).addClass("night");		   
+        }
+     });
+     
+    });
+    
+    $(document).ready(function(){
+       
+    $( '#showTitleSix' ).hover(function() {
+        if($( '#titleSix' ).is(":visible")){
+            $( '#titleSix' ).fadeOut(200);
+            $(".highlight, .active").css({
+            "color": 'rgb(100, 70, 255)'
+        }).removeClass("night");		   
+        } else{
+            $( '#titleSix' ).fadeIn();
+            $(".highlight, .active").css({
+            "color": "black"
+        }).addClass("night");		   
+        }
+     });
+     
+    });		
+    
+    
+    $(document).ready(function(){
+       
+    $( '#showKosmos' ).hover(function() {
+        if($( '#kosmos' ).is(":visible")){
+            $( '#kosmos' ).fadeOut(200);
+            $(".highlight, .active").css({
+            "color": 'rgb(100, 70, 255)'
+        }).removeClass("night");		   
+        } else{
+            $( '#kosmos' ).fadeIn();
+            $(".highlight, .active").css({
+            "color": "black"
+        }).addClass("night");		   
+        }
+     });
+     
+    });		
+            
